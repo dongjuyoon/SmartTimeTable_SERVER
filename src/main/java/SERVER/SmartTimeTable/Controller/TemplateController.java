845023f6a972bf479a984df62bb843f546b66a10
DONGJU <antdny2280@naver.com>
2024-10-23 20:32:06 +0900

    찐 테스트 시작

---
+++ src/main/java/SERVER/SmartTimeTable/Controller/TemplateController.java
@@ -0,0 +1,27 @@
+package SERVER.SmartTimeTable.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TemplateController {
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup"; // signup.html 파일을 반환
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // login.html 파일을 반환
+    }
+
+    @GetMapping("/findId")
+    public String findId() {
+        return "findId"; // login.html 파일을 반환
+    }
+
+    @GetMapping("/findPassword")
+    public String findPassword() {
+        return "findPassword"; // login.html 파일을 반환
+    }
+}
