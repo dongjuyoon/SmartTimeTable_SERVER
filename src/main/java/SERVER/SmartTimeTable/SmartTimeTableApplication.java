91741ed5046aed788342adb4e0227b73a6684a07
Yunho Bae <uuuno130104@naver.com>
2024-11-24 11:34:50 +0900

    완료

--- src/main/java/SERVER/SmartTimeTable/SmartTimeTableApplication.java
+++ src/main/java/SERVER/SmartTimeTable/SmartTimeTableApplication.java
@@ -9,6 +9,7 @@ public class SmartTimeTableApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmartTimeTableApplication.class, args);
 //푸시 제데로 됐는지 확인용
+		//완료
 	}
 
 }
